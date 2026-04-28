8ba1be0ad25d013d49f8c1bb64e4c731db8351c2
Vivaldo Marques <vivaldomarques74@gmail.com>
2026-04-27 22:03:27 -0300

    Ocultar sidebar na rota presenca

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,29 @@
+"use client"
+
 import "./globals.css"
 import Sidebar from "@/components/sidebar"
+import { usePathname } from "next/navigation"
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname()
+
+  const semMenu =
+    pathname === "/presenca"
+
   return (
     <html lang="pt-br">
       <body className="flex bg-black text-white">
-        <Sidebar />
+
+        {!semMenu && <Sidebar />}
+
         <main className="flex-1 p-6">
           {children}
         </main>
+
       </body>
     </html>
   )
